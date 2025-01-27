0890fb8b4110b268d3e1679c6037eaead9f5d114
gurpreetsinghXarterian <gurpreet.singh@xarterian.com>
2025-01-27 10:31:22 +0530

    remove console

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const authenticateToken = () => {
 return(req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-console.log("haveToken",token)
+
     if (token == null) return res.status(401).send({ title: 'Token Missing', message: 'Not Authorized' });
 
     jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
